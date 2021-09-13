72cd8452472e76ec094cf379973d76968c6c0f4b
Karlo Verde <karlo.verde.1996@gmail.com>
2021-09-12 23:47:50 -0500

    refactor: use new API
    - Directly throw errors.

--- lib/src/commands/x/rm/rm.dart
+++ lib/src/commands/x/rm/rm.dart
@@ -9,7 +9,7 @@ class RmCommand extends Command<void> {
   /// {@macro rm_cmd}
   RmCommand();
 
-  @override
+  @override // coverage:ignore-line
   String get description => 'Remove a set of files and folders.';
 
   @override
@@ -20,38 +20,30 @@ class RmCommand extends Command<void> {
 
   @override
   Future<void> run() async {
-    try {
-      final _argResults = ArgumentError.checkNotNull(argResults);
-      final args = _argResults.arguments;
-      if (args.isEmpty) {
-        stdout.writeln(
-          'A set of file and/or directory paths should be provided.',
-        );
+    final _argResults = ArgumentError.checkNotNull(argResults);
+    final args = _argResults.arguments;
+    if (args.isEmpty) {
+      throw ArgumentError(
+        'A set of file and/or directory paths should be provided.',
+      );
+    }
+    for (final elementPath in args) {
+      final elementType = FileSystemEntity.typeSync(elementPath);
+
+      // The element can only be a folder or a file.
+      // ignore: exhaustive_cases
+      switch (elementType) {
+        case FileSystemEntityType.directory:
+          final dir = Directory(elementPath);
+          dir.deleteSync(recursive: true);
+          break;
+        case FileSystemEntityType.file:
+          final file = File(elementPath);
+          file.deleteSync(recursive: true);
+          break;
+        case FileSystemEntityType.notFound:
+          throw StateError('The element <$elementPath> does not exist.');
       }
-      for (final elementPath in args) {
-        final elementType = FileSystemEntity.typeSync(elementPath);
-        switch (elementType) {
-          case FileSystemEntityType.directory:
-            final dir = Directory(elementPath);
-            dir.deleteSync(recursive: true);
-            break;
-          case FileSystemEntityType.file:
-            final file = File(elementPath);
-            file.deleteSync(recursive: true);
-            break;
-          case FileSystemEntityType.notFound:
-            stdout.writeln('The element <$elementPath> does not exist.');
-            break;
-          default:
-            throw UnsupportedError(
-              'Unsupported element type: $elementType. '
-              "The next paths in the provided set won't be processed.",
-            );
-        }
-      }
-    } catch (e) {
-      stdout.write('$e');
-      exitCode = 1;
     }
   }
 }
