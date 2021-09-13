2a11ff7938c3754aaaaf3de62ead211d3e1d2e6a
Karlo Verde <karlo.verde.1996@gmail.com>
2021-09-13 00:14:11 -0500

    fix: print error msg only

--- bin/x.dart
+++ bin/x.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+
 import 'package:cov_utils/cov_utils.dart' show x;
 
-Future<void> main(List<String> arguments) async => x(arguments);
+Future<void> main(List<String> arguments) async => x(arguments).catchError(
+      (Object e) {
+        stdout.write(e);
+        exit(1);
+      },
+    );
