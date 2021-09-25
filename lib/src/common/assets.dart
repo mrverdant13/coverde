59f88a0d342e99d5c68f75feb5bf166974d307c1
Karlo Verde <karlo.verde.1996@gmail.com>
2021-09-25 02:49:21 -0500

    feat: create assets

---
+++ lib/src/common/assets.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
+/// CLI folder path.
+final _covUtilsPath = Platform.script.pathSegments
+    .takeWhile((segment) => segment != 'bin')
+    .reduce(p.join);
+
+/// CLI assets folder path.
+final assetsPath = p.join(
+  _covUtilsPath,
+  'assets',
+);
