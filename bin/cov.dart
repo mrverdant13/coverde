4f7478af85a25c6473815582ffdc4169d5633a74
Karlo Verde <karlo.verde.1996@gmail.com>
2021-09-24 20:06:35 -0500

    chore: rename barrel file

--- bin/cov.dart
+++ bin/cov.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:coverde/cov_utils.dart' show cov;
+import 'package:coverde/coverde.dart' show cov;
 
 Future<void> main(List<String> arguments) async => cov(arguments).catchError(
       (Object e) {
