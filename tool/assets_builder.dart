71d8e79874be42252fb56d0ac442682aa1aaab98
Karlo Verde <karlo.verde.1996@gmail.com>
2021-10-02 05:19:34 -0500

    chore: create and setup assets builder

---
+++ tool/assets_builder.dart
@@ -0,0 +1,100 @@
+import 'package:build/build.dart';
+import 'package:path/path.dart' as p;
+
+class AssetsBuilder implements Builder {
+  static const _inputDirSegments = ['assets'];
+  static final _inputDir = _inputDirSegments.reduce(p.join);
+
+  static const _outputDirSegments = ['lib', 'src', 'assets'];
+  static final _outputDir = _outputDirSegments.reduce(p.join);
+
+  static const _outputExtension = '.asset.dart';
+
+  static final _inputPath = '$_inputDir/{{}}';
+  static final _outputPath = '$_outputDir/{{}}$_outputExtension';
+
+  @override
+  final buildExtensions = {
+    _inputPath: [_outputPath],
+  };
+
+  @override
+  Future<void> build(BuildStep buildStep) async {
+    final origin = buildStep.inputId;
+    final assetFilename = p.basename(origin.path);
+    final assetContent = await buildStep.readAsBytes(origin);
+
+    final sb = StringBuffer()
+      ..writeln('/// The filename of the')
+      ..writeln('/// `$assetFilename` asset')
+      ..writeln("const ${assetFilename.asCamelCase}Filename = '''")
+      ..write(assetFilename)
+      ..writeln("''';")
+      ..writeln()
+      ..writeln('/// The collection of bytes that represent the content of the')
+      ..writeln('/// `$assetFilename` asset.')
+      ..writeln('const ${assetFilename.asCamelCase}Bytes = <int>[');
+    for (final b in assetContent) {
+      sb.writeln('  $b,');
+    }
+    sb.writeln('];');
+
+    final destination = AssetId(
+      origin.package,
+      p.join(
+        _outputDir,
+        p.relative(origin.path, from: _inputDir),
+      ),
+    ).addExtension(_outputExtension);
+
+    await buildStep.writeAsString(destination, sb.toString());
+  }
+}
+
+extension ExtendedString on String {
+  static const symbolSet = {' ', '.', '/', '_', r'\', '-'};
+  static final upperAlphaRegex = RegExp('[A-Z]');
+
+  List<String> get words {
+    final sb = StringBuffer();
+    final words = <String>[];
+    final isAllCaps = toUpperCase() == this;
+
+    for (var i = 0; i < length; i++) {
+      final char = this[i];
+      final nextChar = i + 1 == length ? null : this[i + 1];
+
+      if (symbolSet.contains(char)) {
+        continue;
+      }
+
+      sb.write(char);
+
+      final isEndOfWord = nextChar == null ||
+          (upperAlphaRegex.hasMatch(nextChar) && !isAllCaps) ||
+          symbolSet.contains(nextChar);
+
+      if (isEndOfWord) {
+        words.add(sb.toString());
+        sb.clear();
+      }
+    }
+
+    return words;
+  }
+
+  String upperCaseFirstLetter(String word) =>
+      '${word.substring(0, 1).toUpperCase()}${word.substring(1).toLowerCase()}';
+
+  String get asCamelCase {
+    if (words.isEmpty) return this;
+    final firstWord = words.first;
+    final pascalCase = [
+      firstWord.toLowerCase(),
+      ...words.skip(1).map(upperCaseFirstLetter),
+    ].join();
+    return pascalCase;
+  }
+}
+
+Builder assetsBuilder(BuilderOptions options) => AssetsBuilder();
