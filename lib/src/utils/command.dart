60bff4e05303bb33ce7384a918e54732fd4f9a9d
Karlo Verde <karlo.verde.1996@gmail.com>
2021-11-24 21:04:44 -0500

    feat: add cmd validation methods

---
+++ lib/src/utils/command.dart
@@ -0,0 +1,41 @@
+import 'package:args/command_runner.dart';
+
+/// Extended utils on the command implementation.
+extension ExtendedCommand on Command {
+  /// Validate command option.
+  String checkOption({
+    required String optionKey,
+    required String optionName,
+  }) {
+    if (argResults == null) usageException('Missing arguments.');
+    final maybeOption = argResults![optionKey] as String?;
+    if (maybeOption == null || (maybeOption is String && maybeOption.isEmpty)) {
+      usageException('The `$optionName` is required.');
+    }
+    return maybeOption;
+  }
+
+  /// Validate command flag.
+  bool checkFlag({
+    required String flagKey,
+    required String flagName,
+  }) {
+    if (argResults == null) usageException('Missing arguments.');
+    final maybeFlag = argResults![flagKey] as bool?;
+    if (maybeFlag == null) usageException('The `$flagName` flag is required.');
+    return maybeFlag;
+  }
+
+  /// Validate coverage value.
+  T checkCoverage<T extends num>({
+    required T coverage,
+    required String valueName,
+  }) {
+    if (coverage.isNegative) {
+      usageException('The $valueName value should be positive.');
+    } else if (coverage > 100) {
+      usageException('The $valueName value should not be greater than 100.');
+    }
+    return coverage;
+  }
+}
