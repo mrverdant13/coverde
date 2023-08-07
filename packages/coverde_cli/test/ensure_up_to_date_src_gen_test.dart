45e5a9200ec1b0ef5a9895a9e639f64d1022c16d
Karlo Verde <karlo.verde.1996@gmail.com>
2023-08-07 00:53:48 -0500

    test(coverde): relative path for generation tests

--- packages/coverde_cli/test/ensure_up_to_date_src_gen_test.dart
+++ packages/coverde_cli/test/ensure_up_to_date_src_gen_test.dart
@@ -2,4 +2,13 @@
 import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
-void main() => test('Ensure up-to-date source generation', expectBuildClean);
+void main() {
+  test(
+    'Ensure up-to-date source generation',
+    () {
+      expectBuildClean(
+        packageRelativeDirectory: 'packages/coverde_cli',
+      );
+    },
+  );
+}
